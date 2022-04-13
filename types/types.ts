a704aace41d8ab98c3e67440044fdf827dedd412
Dani Almaraz <dtalmaraz@gmail.com>
2022-04-13 09:40:48 -0700

    chromogen chrome extension source code

---
+++ types/types.ts
@@ -0,0 +1,10 @@
+// export interface Connections {
+//   [tabId: string]: any;
+// }
+
+// export interface Message {
+//   tabId: string;
+//   action: string;
+// }
+
+// This page intentionally left blank
